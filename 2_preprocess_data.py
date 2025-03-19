88312314ca6717ab8b00f6431300be629b82a708
DY112 <dongyoung.kim@yonsei.ac.kr>
2025-03-19 19:04:05 +0900

    Add MCC masking subset option

--- 2_preprocess_data.py
+++ 2_preprocess_data.py
@@ -16,6 +16,8 @@ SQUARE_CROP = True      # Trim left/right sides of the image so that it is squar
 SIZE = 512              # Size of train/val image. If None, keep the original resolution.
 TEST_SIZE = 256         # Size of test image. If None, keep the original resolution.
 CAMERA = "galaxy"       # LSMI subset camera
+MCC_MASKING_SUBSET = {'train'}  # Apply MCC masking to these subsets. options: {'train', 'val', 'test'}
+
 if SIZE != None:
     DST_ROOT = CAMERA + "_" + str(SIZE)
 else:
@@ -76,27 +78,29 @@ for key, places in split_data.items():
             img_wb = img / illum_map
             img_wb = np.clip(img_wb, 0, SATURATION - BLACK_LEVEL)
 
-            # apply MCC mask to original image, GT image (training set)
-            if split == "train":
-                mask = np.ones_like(img[:,:,0:1], dtype='float32')
-                mcc1 = (np.float32(meta_data[place]["MCCCoord"]["mcc1"]) / 2).astype(int)
-                mcc2 = (np.float32(meta_data[place]["MCCCoord"]["mcc2"]) / 2).astype(int)
-                mcc3 = (np.float32(meta_data[place]["MCCCoord"]["mcc3"]) / 2).astype(int)
-                mcc_list = [mcc1.tolist(),mcc2.tolist(),mcc3.tolist()]
-                for mcc in mcc_list:
-                    contour = np.array([[mcc[0]],[mcc[1]],[mcc[2]],[mcc[3]]]).astype(int)
-                    cv2.drawContours(mask, [contour], 0, (0), -1)
+            # apply MCC mask to original image, GT image
+            mask = np.ones_like(img[:,:,0:1], dtype='float32')
+            mcc1 = (np.float32(meta_data[place]["MCCCoord"]["mcc1"]) / 2).astype(int)
+            mcc2 = (np.float32(meta_data[place]["MCCCoord"]["mcc2"]) / 2).astype(int)
+            mcc3 = (np.float32(meta_data[place]["MCCCoord"]["mcc3"]) / 2).astype(int)
+            mcc_list = [mcc1.tolist(),mcc2.tolist(),mcc3.tolist()]
+            for mcc in mcc_list:
+                contour = np.array([[mcc[0]],[mcc[1]],[mcc[2]],[mcc[3]]]).astype(int)
+                cv2.drawContours(mask, [contour], 0, (0), -1)
+            if split in MCC_MASKING_SUBSET:
                 img = img * mask
                 img_wb = img_wb * mask
 
-            # Crop original image, GT image, mixmap
-            if SQUARE_CROP or split == "test":
+            # Crop original image, GT image, mixmap, illum_map, mask
+            if SQUARE_CROP:
                 height, width, _ = img.shape
                 w_start = int(width/2) - int(height/2)
                 w_end = w_start + height
                 img = img[:,w_start:w_end,:]
                 img_wb = img_wb[:,w_start:w_end,:]
                 mixmap = mixmap[:,w_start:w_end,:]
+                illum_map = illum_map[:,w_start:w_end,:]
+                mask = mask[:,w_start:w_end,:]
                 # prevent negative mask value interpolation if ZERO_MASK is negative value
                 mixmap = np.where(mixmap==ZERO_MASK,0,mixmap)
             
@@ -110,19 +114,17 @@ for key, places in split_data.items():
                 img = cv2.resize(img, dsize=(resize_len,resize_len), interpolation=cv2.INTER_LINEAR).astype('uint16')
                 img_wb = cv2.resize(img_wb, dsize=(resize_len,resize_len), interpolation=cv2.INTER_LINEAR).astype('uint16')
                 mixmap = cv2.resize(mixmap, dsize=(resize_len,resize_len), interpolation=cv2.INTER_LINEAR)
+                illum_map = cv2.resize(illum_map, dsize=(resize_len,resize_len), interpolation=cv2.INTER_LINEAR)
+                mask = cv2.resize(mask, dsize=(resize_len,resize_len), interpolation=cv2.INTER_LINEAR)
             else:
                 img = img.astype('uint16')
                 img_wb = img_wb.astype('uint16')
                 
-            # save image, GT image, MCC mask, mixmap
+            # save image, GT image, illum_map, MCC mask, mixmap
             cv2.imwrite(os.path.join(dst_path,file), cv2.cvtColor(img,cv2.COLOR_RGB2BGR))
             cv2.imwrite(os.path.join(dst_path,fname+"_gt.tiff"), cv2.cvtColor(img_wb,cv2.COLOR_RGB2BGR))
-            if split == "train":
-                if SQUARE_CROP:
-                    mask = mask[:,w_start:w_end,:]
-                if resize_len != None:
-                    mask = cv2.resize(mask, dsize=(resize_len,resize_len), interpolation=cv2.INTER_LINEAR)
-                cv2.imwrite(os.path.join(dst_path,place+"_mask.png"), mask)
+            np.save(os.path.join(dst_path,fname+"_illum.npy"), illum_map)
+            cv2.imwrite(os.path.join(dst_path,place+"_mask.png"), mask)
             if len(illum_count) != 1:
                 np.save(os.path.join(dst_path,fname), mixmap)
 
