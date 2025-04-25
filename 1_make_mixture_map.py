fixed underflow related bug when calculating mixmap for _123 scens

1_make_mixture_map.py
@@ -464,8 +464,8 @@ def get_illumination_map(place, placeInfo):
         coefficient_3 = np.where(denominator_123==0, ZERO_MASK, coefficient_3)
         coefficient_2 = coefficient_2.clip(0, 1)
         coefficient_3 = coefficient_3.clip(0, 1)
-        coefficient_1 = np.where(denominator_123==0, ZERO_MASK, 1 - coefficient_2 - coefficient_3)
-        coefficient_1 = coefficient_1.clip(0, 1)
+        coefficient_1 = np.clip(1 - coefficient_2 - coefficient_3, 0, 1)
+        coefficient_1 = np.where(denominator_123==0, ZERO_MASK, coefficient_1)
         coefficient_map = np.stack((coefficient_1, coefficient_2, coefficient_3), axis=-1)
         np.save(src_path + img_123_name, coefficient_map)
 